a720f9c817e2bd96ce1b1ae54812a4edd86f6134
Lester <32542078+lhes23@users.noreply.github.com>
2023-08-09 11:12:08 +0800

    add equal formula

--- src/actions/serverActions.ts
+++ src/actions/serverActions.ts
@@ -1,13 +1,23 @@
 "use server"
 
 import { baseUrl } from "@/lib/baseUrl"
-import { revalidatePath, revalidateTag } from "next/cache"
+import { revalidateTag } from "next/cache"
 
 export const addBudgetHandler = async (e: FormData) => {
+  const amnt = e.get("budgetAmount")?.toString()
+  const amountTotal = amnt?.startsWith("=")
+    ? amnt
+        ?.split("=")[1]
+        .toString()
+        .split("+")
+        .map((amn) => parseInt(amn))
+        .reduce((a, c) => a + c)
+    : amnt
+  console.log({ amountTotal })
   const data = {
     budgetName: e.get("budgetName")?.toString(),
     budgetType: e.get("budgetType")?.toString(),
-    budgetAmount: e.get("budgetAmount")?.toString()
+    budgetAmount: amountTotal?.toString()
   }
 
   const res = await fetch(`${baseUrl}/api/budgets`, {
