3ae1617346eb98d217746329701342495b5da8f9
Lester <32542078+lhes23@users.noreply.github.com>
2023-08-12 23:37:32 +0800

    chnage button text size

--- src/components/ButtonComp.tsx
+++ src/components/ButtonComp.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const ButtonComp = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded">
+    <div className="bg-purple-500 hover:bg-purple-400 text-white font-bold p-2 border-b-4 border-purple-700 hover:border-purple-500 rounded">
       {children}
     </div>
   )
