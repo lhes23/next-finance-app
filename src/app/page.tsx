5a0a24fc7a8df283ccd4d196b56bdb811726e1a8
Lester <32542078+lhes23@users.noreply.github.com>
2023-08-17 09:54:26 +0800

    add bg on login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Login from "../components/Login"
 
 export default async function Home() {
   return (
-    <main className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+    <main className="flex items-center min-h-screen p-6 bg-[url('/imgs/bg-mountains.jpg')] bg-no-repeat bg-cover bg-fixed">
       <Login />
     </main>
   )
