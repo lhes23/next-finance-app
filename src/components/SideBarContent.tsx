c1b64c5d90bce38d1870803ddb99bd32493958d7
Lester <32542078+lhes23@users.noreply.github.com>
2023-08-17 19:23:18 +0800

    add bg color on active side menu

--- src/components/SideBarContent.tsx
+++ src/components/SideBarContent.tsx
@@ -1,4 +1,6 @@
 "use client"
+import { setActiveSideMenu } from "@/redux/dashboardSlice"
+import { useAppDispatch, useAppSelector } from "@/redux/store"
 import Link from "next/link"
 
 const SideBarContent = ({
@@ -8,6 +10,12 @@ const SideBarContent = ({
   title: string
   sidebarLinks: { name: string; url: string; icon: any }[]
 }) => {
+  const activeSideMenu = useAppSelector(
+    (state) => state.dashboardSlice.activeSideMenu
+  )
+  const dispatch = useAppDispatch()
+
+  console.log({ activeSideMenu })
   return (
     <div className={`py-4`}>
       <h2 className={`ml-6 text-lg font-bold`}>{title}</h2>
@@ -16,16 +24,23 @@ const SideBarContent = ({
         {sidebarLinks.map((side, i) => (
           <li
             key={i}
-            className={`relative px-6 py-3 hover:shadow-lg hover:text-xl hover:bg-purple-400 `}
+            className={`relative px-6 py-3 hover:shadow-lg hover:text-xl hover:bg-purple-400 ${
+              activeSideMenu == side.name ? "bg-purple-500" : ""
+            }`}
           >
             <Link
               className="flex items-center w-full transition-colors duration-150"
               href={side.url}
+              onClick={() => dispatch(setActiveSideMenu(side.name))}
             >
-              <span
-                className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                aria-hidden="true"
-              ></span>
+              {activeSideMenu == side.name ? (
+                <span
+                  className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                  aria-hidden="true"
+                ></span>
+              ) : (
+                ""
+              )}
 
               {side.icon}
               <span className="ml-4 text-sm font-semibold">{side.name}</span>
