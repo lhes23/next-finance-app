d7e2be31f5489d80d977e2c05e23367d99d5d8aa
Lester <32542078+lhes23@users.noreply.github.com>
2023-09-01 21:36:25 +0800

    able to add on edit

--- src/app/api/budgets/[id]/route.ts
+++ src/app/api/budgets/[id]/route.ts
@@ -16,12 +16,20 @@ export const GET = async (req: Request, { params }: IParams) => {
 export const PUT = async (req: Request, { params }: IParams) => {
   const { id } = params
   const data = await req.json()
+  let budgAmt = data.budgetAmount
+  if (budgAmt.includes("+")) {
+    budgAmt = data.budgetAmount
+      .split("+")
+      .map((c: string) => Number(c))
+      .reduce((a: number, c: number) => a + c)
+      .toString()
+  }
   const budget = await prisma.budget.update({
     where: { id },
     data: {
       budgetName: data.budgetName,
       budgetType: data.budgetType,
-      budgetAmount: data.budgetAmount
+      budgetAmount: budgAmt
     }
   })
   return NextResponse.json(budget)
