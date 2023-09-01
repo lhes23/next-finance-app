0b2b946d9f83cb69c1b540476fbab5567caa118e
Lester <32542078+lhes23@users.noreply.github.com>
2023-09-01 14:42:49 +0800

    make the  yearly budget reactselect manual

--- src/components/YearlyTable.tsx
+++ src/components/YearlyTable.tsx
@@ -21,12 +21,19 @@ const YearlyTable = () => {
     (ied) => ied.year === year
   )
 
-  const iedOptions: ISelectOption[] = incomesExpensesData.map((ied) => {
-    return {
-      label: ied.year,
-      value: ied.year
-    }
-  })
+  // const iedOptions: ISelectOption[] = incomesExpensesData.map((ied) => {
+  //   return {
+  //     label: ied.year,
+  //     value: ied.year
+  //   }
+  // })
+
+  const iedOptions: ISelectOption[] = [
+    { label: "2023", value: 2023 },
+    { label: "2024", value: 2024 },
+    { label: "2025", value: 2025 },
+    { label: "2026", value: 2026 }
+  ]
 
   return (
     <>
