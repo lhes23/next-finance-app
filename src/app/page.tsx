ffe6fd3393900f4ddd604e359ad2d5a60550704d
Lester <32542078+lhes23@users.noreply.github.com>
2023-09-03 22:44:47 +0800

    add redirect when logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { authOptions } from "@/lib/authOptions"
 import Login from "../components/Login"
+import { getServerSession } from "next-auth/next"
+import { redirect } from "next/navigation"
 
 export default async function Home() {
+  const session = await getServerSession(authOptions)
+  if (session) return redirect("/dashboard")
   return (
     <main className="flex items-center min-h-screen p-6 bg-[url('/imgs/bg-mountains.jpg')] bg-no-repeat bg-cover bg-local md:bg-fixed">
       <Login />
