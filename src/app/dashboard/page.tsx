c52c590397fe397d645e9a84c5ead2c2f15c8d5d
Lester <32542078+lhes23@users.noreply.github.com>
2023-09-03 22:31:55 +0800

    create a component for Income and Expense Table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,14 @@
-"use client"
 import React from "react"
 import PageComponent from "@/components/PageComponent"
 import DashboardCards from "@/components/DashboardCards"
 import DashboardCharts from "@/components/DashboardCharts"
-import IncomeExpenseTable from "@/components/IncomeExpenseTable"
-import { useAppSelector } from "@/redux/store"
+import { getServerSession } from "next-auth/next"
+import IncomeAndExpense from "@/components/IncomeAndExpense"
+import { authOptions } from "@/lib/authOptions"
 
-const DashboardPage = () => {
-  let all_budgets = useAppSelector(
-    (state) => state.budgetSliceReducer.allBudgets
-  )
-
-  all_budgets = all_budgets.filter(
-    (budget) =>
-      new Date(budget.createdAt).getFullYear() === new Date().getFullYear() &&
-      new Date(budget.createdAt).getMonth() === new Date().getMonth() &&
-      new Date(budget.createdAt).getMonth()
-  )
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions)
+  console.log({ session })
 
   return (
     <>
@@ -28,7 +20,7 @@ const DashboardPage = () => {
         <DashboardCharts />
 
         {/* New Table */}
-        <IncomeExpenseTable all_budgets={all_budgets} />
+        <IncomeAndExpense />
       </PageComponent>
     </>
   )
