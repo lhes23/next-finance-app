e4079b5603246711e60009c28f5f6bfa8f1308b6
Lester <32542078+lhes23@users.noreply.github.com>
2023-09-04 12:48:26 +0800

    use SessionProvider

--- src/redux/AppProvider.tsx
+++ src/redux/AppProvider.tsx
@@ -2,9 +2,14 @@
 import React from "react"
 import { Provider } from "react-redux"
 import { store } from "./store"
+import { SessionProvider } from "next-auth/react"
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <SessionProvider>{children}</SessionProvider>
+    </Provider>
+  )
 }
 
 export default AppProvider
