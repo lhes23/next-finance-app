9a98c3dc33266bdd8e9f3fac898147c81acf1fc2
Lester <32542078+lhes23@users.noreply.github.com>
2023-09-13 20:26:22 +0800

    fix spacing

--- src/components/YearlyTableRow.tsx
+++ src/components/YearlyTableRow.tsx
@@ -7,15 +7,15 @@ const YearlyTableRow = ({
   incomeExpenseRow: IIncomesExpensesData
 }) => {
   const cashFlow = incomeExpenseRow.income - incomeExpenseRow.expense
-  const textColor = cashFlow > 0 ? "text-green-500" : "text-red-500"
+  const textColor = cashFlow > 0 ? "text-green-800" : "text-red-500"
 
   return (
     <>
       <tr className="text-sm">
-        <td className="px-4 py-3">{incomeExpenseRow.month}</td>
-        <td className="px-4 py-3">₱ {incomeExpenseRow.income}</td>
-        <td className="px-4 py-3">₱ {incomeExpenseRow.expense}</td>
-        <td className={`px-4 py-3 ${textColor}`}>₱ {cashFlow}</td>
+        <td className="px-3 py-3">{incomeExpenseRow.month}</td>
+        <td className="px-3 py-3">₱ {incomeExpenseRow.income}</td>
+        <td className="px-3 py-3">₱ {incomeExpenseRow.expense}</td>
+        <td className={`px-3 py-3 ${textColor}`}>₱ {cashFlow}</td>
       </tr>
     </>
   )
